17c7aa555c303197b204d0e9da9475fe8cb81f7e
Jørgen Vatle <Jorgen@WcAServices.net>
2019-04-11 13:20:25 +0300

    Add Warning object interface to MinFraudResponse.

--- src/api/MinFraudResponse.ts
+++ src/api/MinFraudResponse.ts
@@ -28,7 +28,44 @@ export default interface MinFraudResponse {
      * This array contains warning objects detailing issues with the request that was sent such as invalid or
      * unknown inputs. It is highly recommended that you check this array for issues when integrating the web service.
      */
-    warnings: any[];
+    warnings: Warning[];
+
+}
+
+/**
+ * Warning object
+ *
+ * @link https://dev.maxmind.com/minfraud/#Warning
+ */
+export interface Warning {
+
+    /**
+     * This value is a machine-readable code identifying the warning. Although more codes may be added in the future.
+     */
+    code: 'BILLING_CITY_NOT_FOUND'      // the billing city could not be found in our database.
+        | 'BILLING_COUNTRY_MISSING'     // billing address information was provided without providing a billing country.
+        | 'BILLING_COUNTRY_NOT_FOUND'   // the billing country could not be found in our database.
+        | 'BILLING_POSTAL_NOT_FOUND'    // the billing postal could not be found in our database.
+        | 'INPUT_INVALID'               // the value associated with the key does not meet the required constraints, e.g., “United States” in a field that requires a two-letter country code.
+        | 'INPUT_UNKNOWN'               // an unknown key was encountered in the request body.
+        | 'IP_ADDRESS_NOT_FOUND'        // the IP address could not be geolocated.
+        | 'SHIPPING_CITY_NOT_FOUND'     // the shipping city could not be found in our database.
+        | 'SHIPPING_COUNTRY_MISSING'    // shipping address information was provided without providing a shipping country.
+        | 'SHIPPING_COUNTRY_NOT_FOUND'  // the shipping country could not be found in our database.
+        | 'SHIPPING_POSTAL_NOT_FOUND'   // the shipping postal could not be found in our database.
+
+    /**
+     * This field provides a human-readable explanation of the warning. The description may change at any time and
+     * should not be matched against.
+     */
+    warning: string;
+
+    /**
+     * A JSON Pointer to the input field that the warning is associated with. For instance, if the warning was about
+     * the billing city, this would be /billing/city. If it was for the price in the second shopping cart item, it
+     * would be /shopping_cart/1/price
+     */
+    input_pointer: string;
 
 }
 
